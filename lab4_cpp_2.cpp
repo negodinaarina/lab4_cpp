a4b8ba1b8b462562d73e3ca96bec4cec3cc892e2
Arina Negodina <66848459+negodinaarina@users.noreply.github.com>
2023-09-16 12:27:26 +0300

    Update lab4_cpp_2.cpp

--- lab4_cpp_2.cpp
+++ lab4_cpp_2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+//Прототип функции Input
 bool Input(int&, int&);
 
 int main()
@@ -15,7 +15,8 @@ int main()
     cout << s;
     return 0;
 }
-
+//Функция принимает ссылки на a и b, просит ввести значение, производит извлечение и проверяет,
+//если оно было произведено не успешно cin.fail() - возвращаем false, иначе - true
 bool Input(int& a, int& b){
     cout << "Enter a\n";
     cin >> a;
